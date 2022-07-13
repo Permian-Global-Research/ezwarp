174dcc340c814ff9791b0aa5c89753907feaa7ad
Hugh Graham <hugh.a.graham@gmail.com>
2022-07-13 12:39:51 +0100

    minor plot updates

--- R/sciplot.R
+++ R/sciplot.R
@@ -9,8 +9,12 @@
 #' @param n Numeric of length 1. The number of breaks/colours to include in the sclae
 #' @param direction Default is 1. Direction of colour palette to reverse use -1.
 #' @param centre Logical. If TRUE, the palette is centred around zero. good for bivariate/split palettes.
-#' @param n_quantile Numeric. if not NULL sets the number of quantile breaks to use for sciplot.SpatRaster
+#' @param n_quantile Numeric. if not NULL sets the number of quantile breaks to use for sciplot.SpatRaster. See details.
 #' @param ... additional args passed to plot.
+#' 
+#' @details 
+#' n_quantile sets quantile breaks for the colour palette. When using breaks, 
+#' terra::plot does not support continuous legend as is the default in stars.
 #' 
 #' @examples 
 #' 
@@ -37,7 +41,6 @@ sciplot.SpatRaster <- function(x, pal="acton", n=256, direction=1, centre=FALSE,
   if (!is.null(n_quantile)){
     brks <- quantile(x[], probs= seq(0,1, length.out=n_quantile))
   }
-  
   
   pal <- scico::scico(n, palette = pal, direction=direction)
   
@@ -104,4 +107,55 @@ include_cols <- function(x, mav, n){
   
   min(which(n_in_class==T)):max(which(n_in_class==T))
 }
+
+
+# quant_pal <- function(x, n=256, n_quantile=25, bias=1, pal='acton', direction=1,...){
+#   mm <- minmax(x)
+#   .range <- abs(mm[2]-mm[1])
+#   quantile_breaks <- function(xs, .n) {
+#     breaks <- quantile(xs, probs = seq(0, 1, length.out = .n)) #%>% 
+#       # as.numeric()
+#     b <- breaks[!duplicated(breaks)] 
+#     b <- data.frame(b)
+#     b$id <- 1:n_quantile
+#     b
+#   }
+#   quants <- (quantile_breaks(x[], n_quantile))
+#   
+#   # qd <- diff(quants)
+#   
+#   # norm.v <- as.integer(.range/n_quantile)
+#   # reps <- c(ceiling(norm.v/qd),1)
+#   
+#   cuts <- as.integer(findInterval(x[], quantile(x[], seq(0, 1, length.out = n_quantile))))
+#   
+#   # match(cuts, quants)
+#   # which(cuts %in% quants)
+#   # Position(f, x, right = FALSE, nomatch = NA_integer)
+#   
+#   x[] <- as.matrix(quants$b[match(cuts, quants$id)])
+#   
+#   # prop <- qd/sum(qd)*n
+#   # p2 <- quantile(prop, probs = seq(0, 1, length.out = n_quantile))%>% 
+#   #   as.numeric() %>% 
+#   #   rev() %>% 
+#   #   ceiling()
+#   # quants/.range
+#   # 
+#   # quant_levels <- lapply(quants, function(.x) (.x-mm[1])/.range) %>%
+#   #   unlist() %>%
+#   #   as.numeric()
+#   
+#   # ql <- as.integer((1-quant_levels)*(length(x[])/n_quantile))
+#   
+#   # as.integer(quantile(1:n, probs = quant_levels))
+#   
+#   # # color_sequence<-seq(mm[1],mm[2],length.out=n+1)
+#   # n_in_class <- hist(x[], breaks=quants, plot=FALSE)$counts
+#   # ql <- min(which(n_in_class==T)):max(which(n_in_class==T))
+#   # ql <- quant_levels*.range
+#   # pal <- scico::scico(n_quantile, palette = pal, direction=direction)
+#   # rep(pal, reps)
+#   x
+# }
 
